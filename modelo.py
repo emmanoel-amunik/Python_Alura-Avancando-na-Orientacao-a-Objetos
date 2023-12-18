d6e123f5e6a22b8281d259134d3ab1d71c6ada5c
emmanoel-amunik <emmanoelamunik@hotmail.com>
2023-12-18 20:18:21 -0300

    02. Removendo duplicacoes com heranca

--- modelo.py
+++ modelo.py
@@ -1,57 +1,43 @@
-class Movie:
+class Program:
+    def __init__(self, name, year):
+        self._name = name.title()
+        self.year = year
+        self._likes = 0
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name.titlle()
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def give_likes(self):
+        self._likes += 1
+
+
+class Movie(Program):
     def __init__(self, name, year, duration):
-        self.__name = name.title()
-        self.year = year
+        super().__init__(name, year)
         self.duration = duration
-        self.__likes = 0
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name.titlle()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def give_likes(self):
-        self.__likes += 1
 
 
-class Serie:
+class Serie(Program):
     def __init__(self, name, year, seasons):
-        self.__name = name.title()
-        self.year = year
+        super().__init__(name, year)
         self.seasons = seasons
-        self.__likes = 0
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name.titlle()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def give_likes(self):
-        self.__likes += 1
 
 
 avengers = Movie("avengers: infinity war", 2018, 160)
 avengers.give_likes()
-print(f"Name: {avengers.name} - Year: {avengers.year} "
-      f"- Duration: {avengers.duration} - Likes: {avengers.likes}")
+print(f"{avengers.name} - {avengers.duration} - {avengers.likes}")
 
 
 atlanta = Serie("atlanta", 2018, 2)
 atlanta.give_likes()
 atlanta.give_likes()
-print(f"Name: {atlanta.name} - Year: {atlanta.year} "
-      f"- Seasons: {atlanta.seasons} - Likes: {atlanta.likes}")
+print(f"{atlanta.name} - {atlanta.seasons} - {atlanta.likes}")
